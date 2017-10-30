9aa99b73d8a7993a2a6d59cba0762740cf1cbb0f
Vlad Dombrovskiy <dambrik53@gmail.com>
2017-10-30 14:58:17 +0200

    Indentation fixes

--- specs/lib/models/point_spec.rb
+++ specs/lib/models/point_spec.rb
@@ -1,7 +1,7 @@
 describe Point do
 
-	context 'methods' do
-		subject { Point.new(1,2) }
+  context 'methods' do
+    subject { Point.new(1,2) }
 
     it 'has start point' do
       subject.must_respond_to :start_point
@@ -12,17 +12,17 @@ describe Point do
     end
   end
 
-	it 'accepts integer arguments' do
-		point = Point.new(2,3)
-		point.start_point.must_equal 2
-		point.end_point.must_equal 3
-	end
+  it 'accepts integer arguments' do
+    point = Point.new(2,3)
+    point.start_point.must_equal 2
+    point.end_point.must_equal 3
+  end
 
-	it 'accespts castable to integer arguments' do
-		point = Point.new('1', 2.0)
-		point.start_point.must_equal 1
-		point.end_point.must_equal 2
-	end
+  it 'accespts castable to integer arguments' do
+    point = Point.new('1', 2.0)
+    point.start_point.must_equal 1
+    point.end_point.must_equal 2
+  end
 
   it 'does not accept point with invalid first argument' do
     proc do
@@ -31,10 +31,11 @@ describe Point do
   end
 
   it 'does not accept point with invalid second argument' do
-    Proc.new do
+    proc do
       Point.new(1, {})
     end.must_raise Exceptions::WrongPointFormat
   end
 end
+
 
 
